d1452359e3b48053443a32f7dbc9733fd67cd6d6
Shawn Wilkinson <me@super3.org>
2016-06-16 12:18:35 -0400

    Create quickstart.sh

---
+++ quickstart.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install build-essential checkinstall libssl-dev git -y
+wget https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh
+sh install.sh
+source ~/.nvm/nvm.sh
+nvm install 4.4.5
+npm install -g storj --unsafe-perm
