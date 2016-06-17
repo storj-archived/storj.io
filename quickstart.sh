b5bbc6ef870f9da400ba2cce87e0d8322dedaece
Shawn Wilkinson <me@super3.org>
2016-06-17 10:32:59 -0400

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,6 +1,6 @@
 apt-get update
-apt-get install build-essential checkinstall libssl-dev git -y
+apt-get install -y build-essential checkinstall libssl-dev git
 wget https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh
-sh install.sh
+. install.sh
 source ~/.nvm/nvm.sh
 nvm install 4.4.5
