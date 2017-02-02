16ac38d63eb58e35ec2192cf25fa23f2e816b890
littleskunk <jens.heimbuerge@googlemail.com>
2017-02-02 16:39:29 +0100

    update to nodejs v6.9.4

--- quickstart.sh
+++ quickstart.sh
@@ -3,6 +3,6 @@ apt-get install -y build-essential checkinstall libssl-dev git
 wget https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh
 . install.sh
 source ~/.nvm/nvm.sh
-nvm install 4.4.5
+nvm install 6.9.4
 npm install -g storj --unsafe-perm
 echo "Storj is now ready to use!"
