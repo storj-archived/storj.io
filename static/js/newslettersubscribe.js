6799c475a55e21d8f1a0f3043b4670ca875d9329
Malcolm Bouzi <mrbouzi@gmail.com>
2021-01-20 02:23:57 -0500

    refactor newletter submit logic (#468)
    * refactor newletter submit logic to add and remove classes instead of add inline styles
    
    * remove console log
    
    * lowercase and trim newsletter submit email input

--- static/js/newslettersubscribe.js
+++ static/js/newslettersubscribe.js
@@ -8,13 +8,13 @@ window.onload = function() {
     button.onclick = function (event) {
         event.preventDefault();
 
-        var inputValue = document.getElementById("mce-EMAIL-newsletter").value;
+        var inputValue = document.getElementById("mce-EMAIL-newsletter").value.toLowerCase().trim();
         var errElm = document.getElementById("mce-error-response-newsletter");
         var succElm = document.getElementById("mce-success-response-newsletter");
 
         if (!inputValue || !inputValue.includes("@")) {
-            setDisplayAttribute(errElm, "flex");
-            setDisplayAttribute(succElm, "none");
+            setDisplayAttribute(errElm, "d-block", "d-none");
+            setDisplayAttribute(succElm, "d-none", "d-block");
 
             return;
         }
@@ -26,14 +26,15 @@ window.onload = function() {
             function() {
                 analytics.track("storj_newsletter", {});
 
-                setDisplayAttribute(errElm, "none");
-                setDisplayAttribute(succElm, "flex");
+                setDisplayAttribute(errElm, "d-none", "d-block");
+                setDisplayAttribute(succElm, "d-block", "d-none");
             },
         );
 
-        function setDisplayAttribute(element, value) {
+        function setDisplayAttribute(element, classAdding, classRemoving) {
             if (element) {
-                element.setAttribute("style", "display: " + value + ";");
+                element.classList.add(classAdding);
+                element.classList.remove(classRemoving);
             }
         }
     }
